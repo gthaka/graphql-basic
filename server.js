f4eb7a3f439158510780518b33b5632941eb59bc
Anthony G <agithaka@live.com>
2022-05-07 03:43:55 +0300

    Random Quotes tweaks

--- server.js
+++ server.js
@@ -12,13 +12,36 @@ const schema = buildSchema(`
     }
 `);
 
+const quotes = [
+    'There is only one happiness in life, to love and be loved.',
+    'All of our technology is completely unnecessary to a happy life.',
+    'The happiness of a man in this life does not consist in the absence but in the mastery of his passions.',
+    'You will never be happy if you continue to search for what happiness consists of. You will never live if you are looking for the meaning of life.',
+    'Very little is needed to make a happy life; it is all within yourself, in your way of thinking.',
+    'There is no way to happiness, happiness is the way.',
+    'Whoever is happy will make others happy, too.',
+    'Thousands of candles can be lit from a single, and the life of the candle will not be shortened. Happiness never decreases by being shared.',
+    'Happiness resides not in possessions, and not in gold, happiness dwells in the soul.',
+    'Thousands of candles can be lighted from a single candle, and the life of the candle will not be shortened. Happiness never decreases by being shared.',
+    'Independence is happiness.',
+    'Action may not always bring happiness; but there is no happiness without action.',
+    'Happiness depends upon ourselves.',
+    'Wisdom is the supreme part of happiness.',
+    'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.',
+    'Happiness can exist only in acceptance.',
+    'Happiness is found in doing, not merely possessing.',
+    'There is no duty we so underrate as the duty of being happy. By being happy we sow anonymous benefits upon the world.',
+    'Friends show their love in times of trouble, not in happiness.',
+    'Love is the master key that opens the gates of happiness.'
+];
+
 // The root provides a resolver function for each API endpoint
 const root = {
     hello: () => {
         return 'Hello Coder 👨‍💻👨🏽‍💻🎉';
     },
     quoteOfTheDay: () => {
-        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+        return quotes[Math.floor(Math.random() * quotes.length)];
     },
     random: () => {
         return Math.random();
